e3d7edc0099e74ba0f41c923f4016fa1023a2e11
Taavi Burns <taavi@taaviburns.ca>
2013-06-22 21:55:30 -0400

    Exposes LightningProposals instead of PosterProposals in the admin.

--- symposion_project/proposals/admin.py
+++ symposion_project/proposals/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from symposion_project.proposals.models import TalkProposal, TutorialProposal, PosterProposal
+from symposion_project.proposals.models import TalkProposal, TutorialProposal, LightningProposal
 
 
 admin.site.register(TalkProposal)
 admin.site.register(TutorialProposal)
-admin.site.register(PosterProposal)
+admin.site.register(LightningProposal)
