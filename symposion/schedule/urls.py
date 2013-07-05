61e5216e21bb4b62822017597f9863585efd0a57
Taavi Burns <taavi@taaviburns.ca>
2013-07-05 11:25:40 -0400

    Maps the default schedule route to a placeholder, and puts the real schedule at a different URL.

--- symposion/schedule/urls.py
+++ symposion/schedule/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls.defaults import url, patterns
+from django.views.generic.simple import direct_to_template
 
 
 urlpatterns = patterns("symposion.schedule.views",
-    url(r"^$", "schedule_conference", name="schedule_conference"),
+    #url(r"^$", "schedule_conference", name="schedule_conference"),
+    url(r"^$", direct_to_template, {
+        "template": "schedule_conference_tba.html"
+    }, name="schedule_conference"),
+    url(r"^beta", "schedule_conference", name="schedule_conference_beta"),
     url(r"^edit/$", "schedule_edit", name="schedule_edit"),
     url(r"^list/$", "schedule_list", name="schedule_list"),
     url(r"^presentations.csv$", "schedule_list_csv", name="schedule_list_csv"),
