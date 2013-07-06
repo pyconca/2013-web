fdecdf8b873a43676a1ff0e71ec924ec9d6106fd
Taavi Burns <taavi@taaviburns.ca>
2013-07-06 07:59:50 -0400

    Adds caching so public access to schedule/conference.json is less likely to take down the site.

--- symposion/schedule/urls.py
+++ symposion/schedule/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls.defaults import url, patterns
+from django.views.decorators.cache import cache_page
 from django.views.generic.simple import direct_to_template
+
+from symposion.schedule.views import schedule_json
 
 
 urlpatterns = patterns("symposion.schedule.views",
@@ -17,5 +20,5 @@ urlpatterns = patterns("symposion.schedule.views",
     url(r"^([\w\-]+)/list/$", "schedule_list", name="schedule_list"),
     url(r"^([\w\-]+)/presentations.csv$", "schedule_list_csv", name="schedule_list_csv"),
     url(r"^([\w\-]+)/edit/slot/(\d+)/", "schedule_slot_edit", name="schedule_slot_edit"),
-    url(r"^conference.json", "schedule_json", name="schedule_json"),
+    url(r"^conference.json", cache_page(300)(schedule_json), name="schedule_json"),
 )
